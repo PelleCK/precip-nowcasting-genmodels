86130671502be637b1f545643a689901507cec4a
Pelle Kools <pkools@cn84.science.ru.nl>
2024-04-22 15:38:44 +0200

    adjust paths in config_DGMR

--- generativemodels/DGMR/config_DGMR.py
+++ generativemodels/DGMR/config_DGMR.py
@@ -1,32 +1,34 @@
+from pathlib import Path
+
 # Variables to set for the specific user
-USER_FOLDER = "" #TODO: define path
-project_FOLDER = 'precipitationnowcasting-generativemodels-highevents/DGMR'
+USER_FOLDER = Path("/vol/knmimo-nobackup/users/pkools/thesis-forecasting")
+PROJECT_FOLDER = USER_FOLDER / "precip-nowcasting-genmodels/DGMR"
 
 # When not rendering a new list with IDs, use the default option as listed below
 basic_IDs_npy = 'list_IDs200621_avg001mm.npy'
 
 # Global variables that point to the correct directory
-path_data = "" #TODO: define path 
+path_data = USER_FOLDER / "data"
 
-path_code = f'{USER_FOLDER}/{project_FOLDER}/'
+path_code = PROJECT_FOLDER
 
-path_project = '//'
+path_project = Path('//')
 
-dir_basic_IDs = path_code + f'/data/{basic_IDs_npy}'
+dir_basic_IDs = path_code / 'data' / basic_IDs_npy
 
-dir_train_IDs = path_code + f'/data/train_split.npy'
-dir_val_IDs = path_code + f'/data/val_split.npy'
-dir_test_IDs = path_code + f'/data/test_split.npy'
+dir_train_IDs = path_code / 'data/train_split.npy'
+dir_val_IDs = path_code / 'data/val_split.npy'
+dir_test_IDs = path_code / 'data/test_split.npy'
 
-dir_rtcor = path_data + 'dataset_rtcor/'
+dir_rtcor = path_data / "rtcor-recent"
 
-dir_prep = 'preprocessed/'
-dir_rtcor_prep = path_data + dir_prep + 'rtcor_prep/'
+dir_prep = 'preprocessed'
+dir_rtcor_prep = path_data / dir_prep / 'rtcor_prep'
 
-dir_labels = path_data + dir_prep + 'rtcor_rain_labels/'
-dir_labels_heavy = path_data + dir_prep + 'rtcor_heavy_rain_labels/'
+dir_labels = path_data / dir_prep / 'rtcor_rain_labels'
+dir_labels_heavy = path_data / dir_prep / 'rtcor_heavy_rain_labels'
 
-prefix_rtcor = 'RAD_NL25_RAC_5M_'
+prefix_rtcor = 'RAD_NL25_RAC_RT_'
 
-temp_dir = ''
-SPROG_dir = ''
+temp_dir = Path('')
+SPROG_dir = Path('')
