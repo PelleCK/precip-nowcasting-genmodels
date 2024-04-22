11d2590c88015f6a5a456a8a70ffda2026be15cc
Pelle Kools <pkools@cn84.science.ru.nl>
2024-04-22 15:39:21 +0200

    adjust paths to work with pathlib as in config_DGMR

--- generativemodels/DGMR/preprocessing/apply_labeling_preprocessing.py
+++ generativemodels/DGMR/preprocessing/apply_labeling_preprocessing.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append('/vol/knmimo-nobackup/users/pkools/thesis-forecasting/precip-nowcasting-genmodels/')
+
 from preprocessing.preprocessing_rtcor import *
 import config_DGMR
 from tqdm import tqdm
@@ -16,7 +19,7 @@ filenames_rtcor = [item for sublist in filenames_rtcor for item in sublist]
 print(f"Total number of listedID radar images: {len(filenames_rtcor)}")
 
 # Run preprocessing steps (files will be saved in the specified folder in the config
-#rtcor2npy(config_DGMR.dir_rtcor, config_DGMR.dir_rtcor_prep, overwrite = False, preprocess = True, filenames = filenames_rtcor)
+rtcor2npy(config_DGMR.dir_rtcor, config_DGMR.dir_rtcor_prep, overwrite = False, preprocess = True, filenames = filenames_rtcor)
 
 #----------
 from os import listdir
