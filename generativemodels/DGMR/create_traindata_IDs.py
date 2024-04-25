26e54861254c993e64abffd411979736f7a017a1
Pelle Kools <pkools@cn84.science.ru.nl>
2024-04-25 14:14:46 +0200

    smaller time range for testing pipeline, and adjust filename accordingly

--- generativemodels/DGMR/create_traindata_IDs.py
+++ generativemodels/DGMR/create_traindata_IDs.py
@@ -9,16 +9,18 @@ print(f"Create splitting sets with corresponding IDs")
 physical_devices = tf.config.list_physical_devices('GPU')
 print("Num GPUs Available: ", len(physical_devices))
 
-start_dt = datetime(2006,1,1,0,0)
-end_dt =  datetime(2021,12,31,0,0)
+start_dt = datetime(2018, 12, 19, 8, 0)
+#start_dt = datetime(2006,1,1,0,0)
+end_dt = datetime(2019, 12, 31, 23, 55)
+#end_dt =  datetime(2021,12,31,0,0)
 print(f"Start date: {start_dt}")
 print(f"End date: {end_dt}")
 
 x_length = 6
 y_length = 1
 filter_no_rain = 'avg0.01mm'
-filename_npy = 'list_IDs200621_avg001mm_train.npy'
-filename = config_DGMR.path_code + f'/data/{filename_npy}'
+filename_npy = f'list_IDs{start_dt.year}{str(end_dt.year)[2:]}_avg001mm_train.npy'
+filename = os.path.join(config_DGMR.path_code, f'data/{filename_npy}')
 
 print(f"Retrieve IDs")
 
@@ -26,6 +28,7 @@ list_IDs = get_list_IDs(start_dt, end_dt, x_length, y_length, filter_no_rain=fil
 print(f"Number of IDs: {len(list_IDs)}")
 #print("Result of IDs:")
 #print(list_IDs)
+list_IDs = np.asarray(list_IDs, dtype='object')
 
 np.save(filename, list_IDs)
 print(f"Saved at location: {filename}")
