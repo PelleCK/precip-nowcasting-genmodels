5f87506663f67d39e91fb035e29cdc682b5721c4
Pelle Kools <pkools@cn84.science.ru.nl>
2024-04-25 14:12:01 +0200

    adjust paths in config_DGMR

--- generativemodels/DGMR/config_DGMR.py
+++ generativemodels/DGMR/config_DGMR.py
@@ -1,34 +1,34 @@
-from pathlib import Path
+import os
 
 # Variables to set for the specific user
-USER_FOLDER = Path("/vol/knmimo-nobackup/users/pkools/thesis-forecasting")
-PROJECT_FOLDER = USER_FOLDER / "precip-nowcasting-genmodels/DGMR"
+USER_FOLDER = "/vol/knmimo-nobackup/users/pkools/thesis-forecasting"
+PROJECT_FOLDER = os.path.join(USER_FOLDER.strip(), "precip-nowcasting-genmodels/generativemodels/DGMR")
 
 # When not rendering a new list with IDs, use the default option as listed below
 basic_IDs_npy = 'list_IDs200621_avg001mm.npy'
 
 # Global variables that point to the correct directory
-path_data = USER_FOLDER / "data"
+path_data = os.path.join(USER_FOLDER.strip(), "data")
 
 path_code = PROJECT_FOLDER
 
-path_project = Path('//')
+path_project = '//'
 
-dir_basic_IDs = path_code / 'data' / basic_IDs_npy
+dir_basic_IDs = os.path.join(path_code.strip(), 'data', basic_IDs_npy.strip('/'))
 
-dir_train_IDs = path_code / 'data/train_split.npy'
-dir_val_IDs = path_code / 'data/val_split.npy'
-dir_test_IDs = path_code / 'data/test_split.npy'
+dir_train_IDs = os.path.join(path_code.strip(), 'data/train_split.npy')
+dir_val_IDs = os.path.join(path_code.strip(), 'data/val_split.npy')
+dir_test_IDs = os.path.join(path_code.strip(), 'data/test_split.npy')
 
-dir_rtcor = path_data / "rtcor-recent"
+dir_rtcor = os.path.join(path_data.strip(), "rtcor-recent")
 
 dir_prep = 'preprocessed'
-dir_rtcor_prep = path_data / dir_prep / 'rtcor_prep'
+dir_rtcor_prep = os.path.join(path_data.strip(), dir_prep.strip('/'), 'rtcor_prep')
 
-dir_labels = path_data / dir_prep / 'rtcor_rain_labels'
-dir_labels_heavy = path_data / dir_prep / 'rtcor_heavy_rain_labels'
+dir_labels = os.path.join(path_data.strip(), dir_prep.strip('/'), 'rtcor_rain_labels')
+dir_labels_heavy = os.path.join(path_data.strip(), dir_prep.strip('/'), 'rtcor_heavy_rain_labels')
 
 prefix_rtcor = 'RAD_NL25_RAC_RT_'
 
-temp_dir = Path('')
-SPROG_dir = Path('')
+temp_dir = ''
+SPROG_dir = ''
