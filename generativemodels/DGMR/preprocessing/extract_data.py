fa0f9f6230fc8ac637646f2c295755a811e70de0
Pelle Kools <pkools@cn84.science.ru.nl>
2024-06-13 14:54:19 +0200

    extract tar files into .h5 files in year folders

--- generativemodels/DGMR/preprocessing/extract_data.py
+++ generativemodels/DGMR/preprocessing/extract_data.py
@@ -16,13 +16,28 @@ def extract_tar_file(tar_path, extract_to):
 for tar_file in root_data_dir.glob('*.tar'):
     # Extract the year from the file name
     file_name = tar_file.name
-    year = file_name[27:31]  # Adjust indices according to your file name structure
+    start_year = file_name[27:31]
+    end_year = file_name[43:47]
 
     # Create a directory for the year if it doesn't exist
-    year_dir = root_data_dir / year
-    year_dir.mkdir(exist_ok=True)
+    start_year_dir = root_data_dir / start_year
+    start_year_dir.mkdir(exist_ok=True)
 
     # Extract the tar file into its corresponding year directory
-    extract_tar_file(tar_file, year_dir)
+    extract_tar_file(tar_file, start_year_dir)
+
+    # check if end year is diff from start year
+    if start_year != end_year:
+        end_year_dir = root_data_dir / end_year
+        end_year_dir.mkdir(exist_ok=True)
+        files = sorted(start_year_dir.glob('*.h5'), reverse=True)
+
+        # move files with end year to end year directory
+        for file in files:
+            if f'_{end_year}' in file.name:
+                os.rename(file, end_year_dir / file.name)
+            else:
+                break
+
 
 print("Data extraction complete.")
